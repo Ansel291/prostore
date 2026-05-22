b4f8a692becf56d7c0752b1e01d63ca8c76ba816
jason <jason@herman-scheer.com>
2026-05-22 10:38:42 -0700

    Session Cart ID Cookie

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import '@/assets/styles/globals.css'
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants'
 import { ThemeProvider } from 'next-themes'
 import { Thermometer } from 'lucide-react'
+import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -33,6 +34,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster closeButton richColors />
         </ThemeProvider>
       </body>
     </html>
---
+++ components/shared/product/add-to-cart.tsx
@@ -0,0 +1,45 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { useRouter } from 'next/navigation'
+import { Plus } from 'lucide-react'
+import { CartItem } from '@/types'
+import { toast } from 'sonner'
+import { addItemToCart } from '@/lib/actions/cart.actions'
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter()
+
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item)
+
+    if (!res.success) {
+      toast.error(res.message)
+      return
+    }
+
+    // Handle success add to cart
+    toast.success(`${item.name} added to cart`, {
+      style: {
+        background: '#fff',
+      },
+      action: {
+        label: 'Go To Cart',
+        onClick: () => router.push('/cart'),
+      },
+      actionButtonStyle: {
+        backgroundColor: 'hsl(var(--primary))',
+        color: 'white',
+        zIndex: 999999,
+        position: 'relative',
+      },
+    })
+  }
+
+  return (
+    <Button className='w-full' type='button' onClick={handleAddToCart}>
+      <Plus /> Add To Cart
+    </Button>
+  )
+}
+
+export default AddToCart
